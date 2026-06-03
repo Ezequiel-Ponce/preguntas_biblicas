ce892570e2e03613b7e28c1b5344990d1bf7094b
Admin <admin@example.com>
2026-06-02 22:39:16 -0600

    Actualizar preguntas biblicas exactas

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "C:\Users\Admin\Desktop\proyecto"
 
-"C:\Program Files\Git\bin\bash.exe" -lc "cd ~/Desktop/proyecto && git remote set-url origin https://github.com/ezequiel-ponce/preguntas_biblicas.git && git add . && git diff --cached --quiet || git commit -m 'Actualizar preguntas biblicas exactas' && git push -u origin master && git push origin master:main"
+"C:\Program Files\Git\bin\bash.exe" -lc "cd ~/Desktop/proyecto && git remote set-url origin https://github.com/ezequiel-ponce/preguntas_biblicas.git && git add . && git diff --cached --quiet || git commit -m 'Actualizar preguntas biblicas exactas' && git push -u origin master && git push --force-with-lease origin master:main"
 
 pause
